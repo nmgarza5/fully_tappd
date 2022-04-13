97ef6ddff57d09dc1d310c1730a227cae1de9205
Nikolas <nikolas.garza5@gmail.com>
2022-04-13 12:43:23 -0700

    styled components installed. PAage wrapper in place

---
+++ react-app/src/components/PageWrapper/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+    width: 100vw;
+    height: auto;
+    min-height: 100vh;
+    background-color: #E1E1E1;
+    display: flex;
+    position: absolute;
+    top: 0;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    `
