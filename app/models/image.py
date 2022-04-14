6d8ebf8ca15eab2a3a1d81b08771859f3d8372df
Nikolas <nikolas.garza5@gmail.com>
2022-04-14 10:15:10 -0700

    update

--- app/models/image.py
+++ app/models/image.py
@@ -7,14 +7,14 @@ class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image = db.Column(db.String(2048), nullable=False)
 
-    brewery_image = db.relationship('Brewery', back_populates="profile_image", cascade="all, delete-orphan")
-    user_image = db.relationship('User', back_populates="profile_image", cascade="all, delete-orphan")
+    brewery_images = db.relationship('Brewery', back_populates="profile_image", cascade="all, delete-orphan")
+    user_images = db.relationship('User', back_populates="profile_image", cascade="all, delete-orphan")
 
 
     def to_dict(self):
         return {
             'id': self.id,
             'image': self.image,
-            'breweries': {brewery.id: brewery.to_dict() for brewery in breweries},
-            'users': {user.id: user.to_dict() for user in users}
+            'breweries': {brewery.id: brewery.to_dict() for brewery in self.brewery_images},
+            'users': {user.id: user.to_dict() for user in self.user_images}
         }
