42c3be9747bcffaf4a415e0beb3cbc40f3cbe2bf
Nikolas <nikolas.garza5@gmail.com>
2022-04-13 21:19:30 -0700

    update images relationships

--- app/models/user.py
+++ app/models/user.py
@@ -15,18 +15,15 @@ class User(db.Model, UserMixin):
     hashed_password = db.Column(db.String(255), nullable=False)
     header = db.Column(db.String(255), nullable=False)
     bio = db.Column(db.Text, nullable=False)
-    profile_image_id = db.Column(
-        db.Integer, db.ForeignKey('images.id'))
+    profile_image_id = db.Column(db.Integer, db.ForeignKey('images.id'))
     # banner_image_id = db.Column(db.Integer, db.ForeignKey('images.id'), nullable=True)
     # FORMAT: 2022-04-02 13:27:25.457314
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(
         db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
-    profile_image = db.relationship(
-        'Image', back_populates="users", cascade="all, delete-orphan", single_parent=True)
-    breweries = db.relationship(
-        'Brewery', back_populates="owner", cascade="all, delete-orphan")
+    profile_image = db.relationship('Image', back_populates="user_image")
+    breweries = db.relationship('Brewery', back_populates="owner")
 
     @property
     def password(self):
