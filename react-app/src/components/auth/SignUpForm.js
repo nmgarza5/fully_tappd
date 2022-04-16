c0661f28948879b47594e8dd6c04ec7739194aeb
Nikolas <nikolas.garza5@gmail.com>
2022-04-15 22:04:57 -0700

    form update

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -79,119 +79,128 @@ const SignUpForm = () => {
                     ))}
                 </div>
                 <div className={styles.fields}>
-                    <div className={styles.small_text}>All fields below are required unless specified.</div>
-                    <div className={styles.field_row}>
-                        <div className={styles.field}>
-                            <i className="fa-solid fa-user"></i>
-                            <input
-                                className={styles.input}
-                                type="text"
-                                name="username"
-                                onChange={(e) => setUsername(e.target.value)}
-                                placeholder='Username'
-                                >
-                            </input>
+                    <div className={styles.upper_fields}>
+                        <div className={styles.small_text}>All fields below are required unless specified.</div>
+                        <div className={styles.field_row}>
+                            <div className={styles.field}>
+                                <i className="fa-solid fa-user"></i>
+                                <input
+                                    className={styles.input}
+                                    type="text"
+                                    name="username"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    placeholder='Username'
+                                    >
+                                </input>
+                            </div>
+                            <div className={styles.field}>
+                                <i className="fa-solid fa-envelope"></i>
+                                <input
+                                    className={styles.input}
+                                    type="text"
+                                    name="email"
+                                    placeholder='Email'
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    ></input>
+                            </div>
                         </div>
-                        <div className={styles.field}>
-                            <i className="fa-solid fa-envelope"></i>
-                            <input
-                                className={styles.input}
-                                type="text"
-                                name="email"
-                                placeholder='Email'
-                                onChange={(e) => setEmail(e.target.value)}
-                            ></input>
+                        <div className={styles.small_text}>Avoid using common words and include a mix of letters and numbers.</div>
+                        <div className={styles.field_row}>
+                            <div className={styles.field}>
+                                <i className="fa-solid fa-lock"></i>
+                                <input
+                                    className={styles.input}
+                                    placeholder="Password"
+                                    type="password"
+                                    name="password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    value={password}
+                                    ></input>
+                            </div>
+                            <div className={styles.field}>
+                                <i className="fa-solid fa-lock"></i>
+                                <input
+                                    className={styles.input}
+                                    placeholder=" Confirm Password"
+                                    type="password"
+                                    name="confirm_password"
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    value={confirm_password}
+                                    required={true}
+                                    ></input>
+                            </div>
+                        </div>
+                        <div className={styles.field_row}>
+                            <div className={styles.field}>
+                                <i className="fa-brands fa-untappd"></i>
+                                <input
+                                    className={styles.input}
+                                    placeholder='First Name'
+                                    type="text"
+                                    name="first_name"
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                    ></input>
+                            </div>
+                            <div className={styles.field}>
+                                <i className="fa-brands fa-untappd"></i>
+                                <input
+                                    className={styles.input}
+                                    placeholder='Last Name'
+                                    type="text"
+                                    name="last_name"
+                                    onChange={(e) => setLastName(e.target.value)}
+                                    ></input>
+                            </div>
                         </div>
                     </div>
-                    <div className={styles.small_text}>Avoid using common words and include a mix of letters and numbers.</div>
-                    <div className={styles.field_row}>
+                    <div className={styles.lower_container}>
                         <div className={styles.field}>
-                            <i className="fa-solid fa-lock"></i>
+                            <label>Birthdate</label>
+                            <DatePicker
+                            className={styles.input}
+                            onChange={setBirthdate}
+                            value={birthdate}
+                            format="MM-yyyy"
+                            required='true'
+
+                            />
+                        </div>
+                        <div className={styles.lower_field}>
+                            Header
                             <input
-                                className={styles.input}
-                                placeholder="Password"
-                                type="password"
-                                name="password"
-                                onChange={(e) => setPassword(e.target.value)}
-                                value={password}
+                            className={styles.lower_input}
+                                type="text"
+                                name="header"
+                                onChange={(e) => setHeader(e.target.value)}
+                                value={header}
+                                placeholder="Write a short header for your Profile (Optional)"
+                                required={true}
                                 ></input>
                         </div>
-                        <div className={styles.field}>
-                            <i className="fa-solid fa-lock"></i>
+                        <div className={styles.lower_field}>
+                            Profile Image
                             <input
-                                className={styles.input}
-                                placeholder=" Confirm Password"
-                                type="password"
-                                name="confirm_password"
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                value={confirm_password}
-                                required={true}
-                            ></input>
-                        </div>
-                    </div>
-                    <div className={styles.field_row}>
-                        <div className={styles.field}>
-                            <i className="fa-brands fa-untappd"></i>
-                            <input
-                                className={styles.input}
-                                placeholder='First Name'
+                            className={styles.lower_input}
                                 type="text"
-                                name="first_name"
-                                onChange={(e) => setFirstName(e.target.value)}
-                            ></input>
+                                name="profile_image"
+                                onChange={(e) => setProfileImage(e.target.value)}
+                                placeholder="Please enter the url to your Profile Image (Optional)"
+                                value={profile_image}
+                                ></input>
                         </div>
-                        <div className={styles.field}>
-                            <i className="fa-brands fa-untappd"></i>
+                        <div className={styles.lower_field}>
+                            Banner Image
                             <input
-                                className={styles.input}
-                                placeholder='Last Name'
+                            className={styles.lower_input}
                                 type="text"
-                                name="last_name"
-                                onChange={(e) => setLastName(e.target.value)}
-                            ></input>
+                                name="banner_image"
+                                onChange={(e) => setBannerImage(e.target.value)}
+                                placeholder="Please enter the url of your Banner Image (Optional)"
+                                value={banner_image}
+                                ></input>
                         </div>
                     </div>
-                    <div>
-                        <label>Birthdate</label>
-                        <DatePicker
-                        onChange={setBirthdate}
-                        value={birthdate}
-                        format="MM-yyyy"
-                        required='true'
-
-                         />
-                    </div>
-                    <div>
-                        <label>Header</label>
-                        <input
-                            type="text"
-                            name="header"
-                            onChange={(e) => setHeader(e.target.value)}
-                            value={header}
-                            required={true}
-                        ></input>
-                    </div>
-                    <div>
-                        <label>Profile Image</label>
-                        <input
-                            type="text"
-                            name="profile_image"
-                            onChange={(e) => setProfileImage(e.target.value)}
-                            placeholder="Please enter the url to your Profile Picture"
-                            value={profile_image}
-                            ></input>
-                    </div>
-                    <div>
-                        <label>Banner Image</label>
-                        <input
-                            type="text"
-                            name="banner_image"
-                            onChange={(e) => setBannerImage(e.target.value)}
-                            placeholder="Please enter the url to your Profile Picture"
-                            value={banner_image}
-                            ></input>
-                    </div>
-                    <div role='button' onClick={handleClick} className={styles.button}>Sign Up</div>
+                    <div role='button' onClick={handleClick} className={styles.button}>Create Account</div>
                 </div>
             </form>
         </div>
