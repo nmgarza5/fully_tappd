7bc259381fe46d17806e378fbf1fce6717588fa5
Nikolas <nikolas.garza5@gmail.com>
2022-04-17 19:18:22 -0700

    info section is done. will continue after review are complete, also did beer styling

--- react-app/src/components/Beer/SingleBeer/index.js
+++ react-app/src/components/Beer/SingleBeer/index.js
@@ -1,62 +1,95 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-// import { useEffect, useState } from "react";
-import { useSelector, useDispatch} from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector} from "react-redux";
 import styles from "./SingleBeer.module.css";
-// import { receiveOneBrewery } from "../../store/breweries";
 import { PageContainer } from "../../PageContainer";
-import { DeleteBeer } from "../DeleteBeer";
-import { showModal, setCurrentModal } from '../../../store/modal';
-import { BeerForm } from "../../../forms/BeerForm";
-
 
 export const SingleBeer = () => {
-    const dispatch = useDispatch()
+    // const dispatch = useDispatch
 	const sessionUser = useSelector((state) => state?.session?.user);
 	const { id } = useParams();
 	const beer = useSelector((state) => state.beer)[`${id}`];
+    const [showMore, setShowMore] = useState(false)
+
+    // const breweryType = (type) => {
+    //     if (type === "micro") return "Micro"
+    //     if (type === "brewpub") return "Brewpub"
+    //     if (type === "regional") return "Regional"
+    //     if (type === "large") return "Large"
+    // }
+
+    // "brewpub" ? "Brewpub" : "regional" ? "Regional" : "large" ? "Large" : null
 
     let isOwner = false;
 	sessionUser && beer?.owner_id === sessionUser.id
-		? (isOwner = true)
-		: (isOwner = false);
-
-    const showBeerForm = () => {
-        dispatch(setCurrentModal(() => (<BeerForm beer={beer} />)));
-        dispatch(showModal());
-        }
-
-    const showDeleteForm = () => {
-        dispatch(setCurrentModal(() => (<DeleteBeer beer_id={beer.id} />)));
-        dispatch(showModal());
-        }
+    ? (isOwner = true)
+    : (isOwner = false);
 
     return (
         <PageContainer>
-            <div className={styles.profile_container}>
-                {isOwner &&
-                <>
-                    <div onClick={showBeerForm} className={styles.button}>Update Beer</div>
-                    <div onClick={showDeleteForm} className={styles.button}>Delete Beer</div>;
-                </>
-                }
-                <div className={styles.card_img}>
-                    <img src={beer.beer_image} alt="beer" />
+            {/* {isOwner &&
+            <>
+                <UpdateBrewery brewery={brewery} />
+                <DeleteBrewery brewery_id={brewery.id} />
+            </>
+            } */}
+            <div className={styles.page}>
+                <div className={styles.left}>
+                    <div className={styles.info}>
+                        {/* <div style={{backgroundImage: `url(${brewery.banner_image})`}}></div> */}
+                        <div className={styles.first_info} >
+                            <div className={styles.card_img}>
+                                <img src={beer.beer_image} alt="beer" />
+                            </div>
+                            <div className={styles.middle}>
+                                <h2>{beer.name}</h2>
+                                <h4>{beer.brewery_name}</h4>
+                                <div>{beer.style}</div>
+                            </div>
+                            <div className={styles.end}>
+                                <div>Total Checkins</div>
+                                <div>Monthly Average</div>
+                                <div># of your checkins</div>
+                                <div># of Favorites</div>
+                            </div>
+                        </div>
+                        {/* <div> */}
+                            <div className={styles.second_info}>
+                                {/* <div className={styles.row}> */}
+                                    <div  className={styles.row}>
+                                        {beer.abv}% ABV
+                                    </div>
+                                    <div className={styles.row}>
+                                        {beer.ibu} IBU
+                                    </div>
+                                    <div className={styles.row}>
+                                        AVG RATING
+                                    </div>
+                                    <div className={styles.row_end}>
+                                        <i className="fa-solid fa-star fa-2x"></i>
+                                    </div>
+                              {/* </div> */}
+                            </div>
+                        {/* </div> */}
+                        <div>
+                            <div className={styles.third_info}>
+                                {!showMore && beer.description.length > 100 ?
+                                    <div className={styles.no_show}>
+                                        {beer.description.slice(0,100)}...
+                                        <div onClick={() => setShowMore(!showMore)}>Show more</div>
+                                     </div>
+                                    :
+                                    <div className={styles.show}>
+                                        {beer.description}
+                                        <div onClick={() => setShowMore(!showMore)}>Show Less</div>
+                                    </div> }
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    {beer.name}
-                </div>
-                <div>
-                    {beer.brewery_name}
-                </div>
-                <div>
-                    {beer.description}
-                </div>
-                <div>
-                    {beer.abv}
-                </div>
-                <div>
-                    {beer.ibu}
+                <div className={styles.right}>
+                    World
                 </div>
             </div>
         </PageContainer>
